fb5ae776de04279c8684abe3224e431160143f20
Daniel Ribeiro França <daniel.ura@gmail.com>
2013-02-19 16:36:01 -0800

    Create modi.js

---
+++ modi.js
@@ -0,0 +1,63 @@
+var Modi = {
+            
+            rules: [],
+            defaultInitName: 'init',
+
+            addRule: function( options ) {
+                this.rules.push( options );
+            },
+
+            _passRules: function( options ) {
+
+                var pass = 0;
+
+                if( options.matchSelector ) {
+                    pass +=  +!$( options.matchSelector ).length;
+                }
+
+                if( options.matchUrl ) {
+                    
+                    var rMatchUrl = options.matchUrl;
+
+                    if( typeof rMatchUrl === 'string' ) {
+                        rMatchUrl = new RegExp( rMatchUrl , "g");
+                    }
+
+                    if( !rMatchUrl.test( window.location.href ) ) {
+                        pass++;
+                    }
+
+                }
+
+                if( typeof options.rule === 'function' ) {
+                    pass += +!options.rule();
+                }
+
+                return !pass;
+            },
+
+            _hasModule: function( rule ) {
+                return !!window[ rule.moduleName ];
+            },
+            
+            execute: function(){
+
+                var rule = {};
+                
+                for( var idxRule in this.rules ) {
+
+                    rule = this.rules[idxRule];
+
+                    if( this._passRules( rule ) ) {
+
+                        if( typeof rule.callback === "function" ) {
+                            rule.callback();
+                        } else if( this._hasModule( rule ) ) {
+                            window[ rule.moduleName ][ this.defaultInitName ]();
+                        }
+                                             
+                    }
+
+                }
+            }
+        };
